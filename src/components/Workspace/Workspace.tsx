b41c6be4a7af46cbdddbd49c8c2c80826bef542e
Dmitry Butakov <sportyounde@MacBook-AirDmitry.local>
2023-06-11 23:09:15 +0400

    added playground space components (PreferenceNav) and common layer cclasses

--- src/components/Workspace/Workspace.tsx
+++ src/components/Workspace/Workspace.tsx
@@ -1,15 +1,16 @@
 import React from "react"
-import ProblemsDescription from "./ProblemsDescription/ProblemsDescription"
 import Split from "react-split"
+import ProblemDescription from "./ProblemsDescription/ProblemsDescription"
+import Playground from "../Playground/Playground"
 
 type WorkspaceProps = {}
 
 const Workspace: React.FC<WorkspaceProps> = () => {
   return (
     <div>
-      <Split className='split' minSize={0}>
-        <ProblemsDescription />
-        <div> some code workspace</div>
+      <Split className="split" minSize={0}>
+        <ProblemDescription />
+        <Playground />
       </Split>
     </div>
   )
